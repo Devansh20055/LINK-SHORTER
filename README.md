55118d0a101f1ca4bc8c5bb1ac983aa152cbb4b1
ToonsHub <67213740+dakshy@users.noreply.github.com>
2021-12-10 16:44:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Droplink-bot
+Make short link by using [Droplink](https://droplink.co/ref/DAKSHYADAV) API key. Made by @dakshy.
+### Installation
+
+#### The Easy Way
+
+<p><a href="https://heroku.com/deploy?template=https://github.com/dakshy/droplink-bot"> <img src="https://img.shields.io/badge/Deploy%20To%20Heroku-blueviolet?style=for-the-badge&logo=heroku" width="200""/></a><br/>
+<a href="https://railway.app/new/template?template=https%3A%2F%2Fgithub.com%2Fdakshy%2Fdroplink-bot&envs=API_KEY%2CAPI_HASH%2CAPI_ID%2CBOT_TOKEN"><img src="https://img.shields.io/badge/Deploy%20To%20Railway-purple?style=for-the-badge&logo=railway" width="200""/></a></p>
+
+##### Required Variables
+
+* `BOT_TOKEN`: Create a bot using [@BotFather](https://t.me/BotFather), and get the Telegram API token.
+* `API_ID`: Get this value from [my.telegram.org](https://my.telegram.org/apps).
+* `API_HASH`: Get this value from [my.telegram.org](https://my.telegram.org/apps).
+* `API_KEY`: Create [Droplink](https://droplink.co/ref/DAKSHYADAV) account then get this value from [Droplink API](https://droplink.co/ref/DAKSHYADAV).
---
+++ bot.py
@@ -0,0 +1,45 @@
+from os import environ
+import aiohttp
+from pyrogram import Client, filters
+
+API_ID = environ.get('API_ID')
+API_HASH = environ.get('API_HASH')
+BOT_TOKEN = environ.get('BOT_TOKEN')
+API_KEY = environ.get('API_KEY', 'e3eddb3e7c5513eee187120fce788ddc4a1a643b')
+
+bot = Client('droplink bot',
+             api_id=API_ID,
+             api_hash=API_HASH,
+             bot_token=BOT_TOKEN,
+             workers=50,
+             sleep_threshold=10)
+
+
+@bot.on_message(filters.command('start') & filters.private)
+async def start(bot, message):
+    await message.reply(
+        f"**Hi {message.chat.first_name}!**\n\n"
+        "I'm a specialised bot for shortening Droplink.co links which can help you earn money by just sharing links. I am made by @ToonsHub2006.")
+
+
+@bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
+async def link_handler(bot, message):
+    link = message.matches[0].group(0)
+    try:
+        short_link = await get_shortlink(link)
+        await message.reply(f'Here is your [`{short_link}`]({short_link})', quote=True)
+    except Exception as e:
+        await message.reply(f'Error: {e}', quote=True)
+
+
+async def get_shortlink(link):
+    url = 'https://droplink.co/api'
+    params = {'api': API_KEY, 'url': link}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, raise_for_status=True) as response:
+            data = await response.json()
+            return data["shortenedUrl"]
+
+
+bot.run()
