d28176d91d07812963b30a286b2525ca3357938d
ToonsHub <67213740+dakshy@users.noreply.github.com>
2021-12-17 08:31:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ async def link_handler(bot, message):
     link = message.matches[0].group(0)
     try:
         short_link = await get_shortlink(link)
-        await message.reply(f'Here is your [`{short_link}`]({short_link})', quote=True)
+        await message.reply(f'Shortened URL: <a href="{short_link}">Click Here</a>', quote=True)
     except Exception as e:
         await message.reply(f'Error: {e}', quote=True)
 
