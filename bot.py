0d066184c5ebacc3193431dda9e7aaf55799c1c5
me is noob <79394737+Devansh20055@users.noreply.github.com>
2022-01-21 19:26:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ async def link_handler(bot, message):
 
 
 async def get_shortlink(link):
-    url = 'https://droplink.co/api'
+    url = 'https://urlshortx.com/api'
     params = {'api': API_KEY, 'url': link}
 
     async with aiohttp.ClientSession() as session:
