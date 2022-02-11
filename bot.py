c15662315871cacbab9b949ce93df2b1bb0999fa
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-02-11 17:54:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ async def start(bot, message):
 @bot.on_message(filters.command('help') & filters.private)
 async def start(bot, message):
       await message.re.ply(
-          f"**__ 👻👻 HeLlo BUDDY 👻👻. Send Your Link 1st❕ \n I Will Send Short Link 👍 \n\n🔰TRY MY OTHER BOTS 🔰  \n✅ ☞ ᴍᴏᴠɪᴇ ꜱᴇᴀʀᴄʜ ʙᴏᴛ ⭐ ☞ @STORE_FOR_ALL_BOT  \n✅ ☞ ᴅʀᴏᴘʟɪɴᴋ ꜱʜᴏʀᴛ ☞@DROPLINK_CREATOR_BOT \n✅ ☞ URLSHORTX ☞ \n✅ ☞ OTHER SHORTER ☞ @STORE_FOR_ALL_BOT  \n✅ ☞ Group Manager Bot ☞ @STORE_FOR_ALL_BOT  \n\n 👁️‍🗨️ Powered by TEAM SILENT KING  __")
+          f"**👻👻 HeLlo {message.chat.first_name} 👻👻. Send Your Link 1st❕ \n I Will Send Short Link 👍 \n\n🔰TRY MY OTHER BOTS 🔰  \n✅ ☞ ᴍᴏᴠɪᴇ ꜱᴇᴀʀᴄʜ ʙᴏᴛ ⭐ ☞ @STORE_FOR_ALL_BOT  \n✅ ☞ ᴅʀᴏᴘʟɪɴᴋ ꜱʜᴏʀᴛ ☞@DROPLINK_CREATOR_BOT \n✅ ☞ URLSHORTX ☞ \n✅ ☞ OTHER SHORTER ☞ @STORE_FOR_ALL_BOT  \n✅ ☞ Group Manager Bot ☞ @STORE_FOR_ALL_BOT  \n\n 🔰 Powered by TEAM SILENT KING 🔰 ")
 
 
 @bot.on_message(filters.command('api') & filters.private)
